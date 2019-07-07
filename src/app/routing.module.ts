dbbbe7dfb7b00e39f8552b2112c3e426d916565d
Sébastien Taton <jimsobieski21@gmail.com>
2019-07-07 17:30:07 +0200

    add auth guard

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeComponent } from './recipes/recipe/recipe.component';
 import { FridgeComponent } from './fridge/fridge.component';
@@ -10,15 +9,17 @@ import { ScanComponent } from './scan/scan.component';
 import { ShoppingComponent } from './shopping/shopping.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo : 'welcome', pathMatch: 'full'},
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'fridge', component: FridgeComponent},
-  { path: 'scan', component: ScanComponent},
-  { path: 'shopping', component: ShoppingComponent},
-  { path: 'recipes', component: RecipesComponent},
-  { path: 'recipe/:id', component: RecipeComponent}
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  { path: 'fridge', component: FridgeComponent, canActivate: [AuthGuard]},
+  { path: 'scan', component: ScanComponent, canActivate: [AuthGuard]},
+  { path: 'shopping', component: ShoppingComponent, canActivate: [AuthGuard]},
+  { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard]},
+  { path: 'recipe/:id', component: RecipeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({

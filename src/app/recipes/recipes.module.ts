93c91dd70f474dce58e6ca36fa81b5fae2a052ba
Sébastien Taton <jimsobieski21@gmail.com>
2019-07-28 23:53:12 +0200

    init scan feature

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -6,11 +6,14 @@ import { CoreModule } from '../core/core.module';
 import { RecipesComponent } from 'src/app/recipes/recipes.component';
 import { RecipeComponent } from 'src/app/recipes/recipe/recipe.component';
 
+import { BottomShoppingListComponent } from 'src/app/shopping/bottom-shopping-list/bottom-shopping-list.component';
+
 @NgModule({
   declarations: [RecipesComponent,RecipeComponent],
   imports: [
     CommonModule,
     CoreModule,
-  ]
+  ],
+  entryComponents: [BottomShoppingListComponent]
 })
 export class RecipesModule { }
--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-scan',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScanComponent implements OnInit {
 
+  @ViewChild("video")
+  public video: ElementRef;
+
+  @ViewChild("canvas")
+  public canvas: ElementRef;
+
+  private stream: MediaStream = null;
+
+  private scanTimer = interval(500);
+
+  public captures: Array<any>;
+
+  public screenSubscribe: any;
+
+  public isRunning: boolean = false;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.captures = [];
+    this.getMediaStream().then((stream) => {
+      this.stream = stream;
+    }, (error) => {
+      console.log(error);
+    })
+  }
+
+  private getMediaStream(): Promise<MediaStream> {
+
+    const video_constraints = { video: true };
+    const _video = this.video.nativeElement;
+    return new Promise<MediaStream>((resolve, reject) => {
+      // (get the stream)
+      return navigator.mediaDevices.
+        getUserMedia(video_constraints)
+        .then(stream => {
+          (<any>window).stream = stream; // make variable available to browser console
+          _video.srcObject = stream;
+          // _video.src = window.URL.createObjectURL(stream);
+          _video.onloadedmetadata = function (e: any) { };
+          _video.play();
+          return resolve(stream);
+        })
+        .catch(err => reject(err));
+    });
+  }
+
+  private capture() {
+    var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+  }
+
+  public start(){
+    this.isRunning = true;
+    this.screenSubscribe = this.scanTimer.subscribe(val => {
+      this.capture()
+    })
+  }
+
+  public stop(){
+    this.isRunning = false;
+    this.screenSubscribe.unsubscribe();
+  }
+
+  public reset(){
+    this.captures = [];
   }
 
 }
